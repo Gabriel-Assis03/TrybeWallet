e171facff521fdcc79c01631580359336ec30e70
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2023-06-15 00:34:56 -0300

    r3 parei no campo de moeda

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -3,7 +3,25 @@ import React, { Component } from 'react';
 class WalletForm extends Component {
   render() {
     return (
-      <div>WalletForm</div>
+      <div>
+        <label>
+          Valor:
+          {' '}
+          <input data-testid="value-input" type="number" placeholder="0" />
+        </label>
+        <label>
+          Moeda:
+          {' '}
+          <select data-testid="currency-input">
+            <option value="valor1">Valor 1</option>
+          </select>
+        </label>
+        <label>
+          Descrição:
+          {' '}
+          <input data-testid="description-input" type="text" placeholder="" />
+        </label>
+      </div>
     );
   }
 }
